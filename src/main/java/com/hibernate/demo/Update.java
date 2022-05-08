3cd6081c17ca264f12ca1dea9826ba3748277644
Kuba1618 <jakub.prusicki1.618@gmail.com>
2022-05-08 04:41:26 +0200

    Update data work

---
+++ src/main/java/com/hibernate/demo/Update.java
@@ -0,0 +1,45 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Update {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try{
+
+            System.out.println("Update a student object ...");
+            Student tempStudent = new Student();
+
+            session.beginTransaction();
+
+            System.out.println("Loading/Retrieving the student ...");
+            tempStudent = session.get(Student.class,2);
+            System.out.println(tempStudent);
+
+            tempStudent.setFirstName("Johny");
+            System.out.println(tempStudent);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+
+        }finally{
+            session.close();
+            factory.close();
+        }
+    }
+
+
+
+}
